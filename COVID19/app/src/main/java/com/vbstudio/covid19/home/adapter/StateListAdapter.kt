5cd51b5322f34cf2d1a2febd240830e5060264b8
Vaibhav Singhal <v_singhal@live.com>
2020-05-26 14:48:07 +0530

    Adding ui updates to state list tab

--- COVID19/app/src/main/java/com/vbstudio/covid19/home/adapter/StateListAdapter.kt
+++ COVID19/app/src/main/java/com/vbstudio/covid19/home/adapter/StateListAdapter.kt
@@ -5,11 +5,6 @@ import androidx.recyclerview.widget.RecyclerView
 import com.vbstudio.covid19.R
 import com.vbstudio.covid19.core.ui.adapter.BaseViewHolder
 import com.vbstudio.covid19.home.dao.RegionItemData
-import kotlinx.android.synthetic.main.item_country_aon.view.*
-import kotlinx.android.synthetic.main.item_country_aon.view.tv_active
-import kotlinx.android.synthetic.main.item_country_aon.view.tv_confirmed
-import kotlinx.android.synthetic.main.item_country_aon.view.tv_deceased
-import kotlinx.android.synthetic.main.item_country_aon.view.tv_recovered
 import kotlinx.android.synthetic.main.item_state_aon.view.*
 
 class StateListAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
@@ -31,19 +26,11 @@ class StateListAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return when (viewType) {
-            FeedRowType.COUNTRY.ordinal -> CountryViewHolder(parent)
-            FeedRowType.STATE.ordinal -> StateViewHolder(parent)
-            else -> StateViewHolder(parent)
-        }
+        return StateViewHolder(parent)
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        if (holder is CountryViewHolder) {
-            holder.onBind(regionItemDataList?.get(position), position)
-        } else if (holder is StateViewHolder) {
-            holder.onBind(regionItemDataList?.get(position), position)
-        }
+        (holder as StateViewHolder).onBind(regionItemDataList?.get(position), position)
     }
 
     override fun getItemCount(): Int {
@@ -55,21 +42,6 @@ class StateListAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
         notifyDataSetChanged()
     }
 
-    class CountryViewHolder(
-        parent: ViewGroup
-    ) : BaseViewHolder<RegionItemData>(
-        parent,
-        R.layout.item_country_aon
-    ) {
-        override fun onBind(data: RegionItemData?, position: Int) {
-            itemView.tv_confirmed.text = data?.confirmedForUI ?: "---"
-            itemView.tv_active.text = data?.activeForUI ?: "---"
-            itemView.tv_recovered.text = data?.recoveredForUI ?: "---"
-            itemView.tv_deceased.text = data?.deathsForUI ?: "---"
-            itemView.tv_data_timestamp.text = data?.lastupdatedtime ?: "---"
-        }
-    }
-
 
     class StateViewHolder(
         parent: ViewGroup
@@ -79,10 +51,10 @@ class StateListAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     ) {
         override fun onBind(data: RegionItemData?, position: Int) {
             itemView.tv_state_name.text = data?.state
-            itemView.tv_confirmed.text = data?.confirmedForUI ?: "---"
-            itemView.tv_active.text = data?.activeForUI ?: "---"
-            itemView.tv_recovered.text = data?.recoveredForUI ?: "---"
-            itemView.tv_deceased.text = data?.deathsForUI ?: "---"
+            itemView.tv_confirmed.text = data?.confirmed ?: "---"
+            itemView.tv_active.text = data?.active ?: "---"
+            itemView.tv_recovered.text = data?.recovered ?: "---"
+            itemView.tv_deceased.text = data?.deaths ?: "---"
             itemView.setBackgroundResource(if (position % 2 == 0) R.color.colorPrimaryDark else R.color.mediumGreen)
         }
     }
